df34ec8fdb1d33815ddca92d67b264c34c087dc8
rbasamoyai <b33gy0sh@gmail.com>
2024-06-13 14:02:03 -0400

    bug: Fixed entities tagged #ritchiesprojectilelib:precise_motion receiving interfering ClientboundMoveEntityPackets
    - Also suppresses ClientboundSetEntityMotionPacket

--- common/src/main/java/rbasamoyai/ritchiesprojectilelib/mixin/ServerEntityMixin.java
+++ common/src/main/java/rbasamoyai/ritchiesprojectilelib/mixin/ServerEntityMixin.java
@@ -1,5 +1,9 @@
 package rbasamoyai.ritchiesprojectilelib.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+
+import net.minecraft.network.protocol.Packet;
 import net.minecraft.server.level.ServerEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.phys.Vec3;
@@ -13,13 +17,15 @@ import rbasamoyai.ritchiesprojectilelib.RPLTags;
 import rbasamoyai.ritchiesprojectilelib.network.ClientboundPreciseMotionSyncPacket;
 import rbasamoyai.ritchiesprojectilelib.network.RPLNetwork;
 
+import java.util.function.Consumer;
+
 @Mixin(ServerEntity.class)
 public class ServerEntityMixin {
 
 	@Shadow @Final private Entity entity;
 
 	@Inject(method = "sendChanges", at = @At("HEAD"))
-	private void ritchiesprojectilelib$sendChanges1(CallbackInfo ci) {
+	private void ritchiesprojectilelib$sendChanges$0(CallbackInfo ci) {
 		if (this.entity.getType().is(RPLTags.PRECISE_MOTION)) {
 			Vec3 pos = this.entity.position();
 			Vec3 vel = this.entity.getDeltaMovement();
@@ -27,5 +33,18 @@ public class ServerEntityMixin {
 			this.entity.hasImpulse = false;
 		}
 	}
+
+    @WrapOperation(method = "sendChanges", at = @At(value = "INVOKE", target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V", ordinal = 2))
+    private void ritchiesprojectilelib$sendChanges$1(Consumer<Packet<?>> instance, Object t, Operation<Void> original) {
+        // Not strictly necessary if the entity does not enable velocity updates (this.trackDelta == true)
+        if (!this.entity.getType().is(RPLTags.PRECISE_MOTION))
+            original.call(instance, t);
+    }
+
+    @WrapOperation(method = "sendChanges", at = @At(value = "INVOKE", target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V", ordinal = 3))
+    private void ritchiesprojectilelib$sendChanges$2(Consumer<Packet<?>> instance, Object t, Operation<Void> original) {
+        if (!this.entity.getType().is(RPLTags.PRECISE_MOTION))
+            original.call(instance, t);
+    }
 
 }
