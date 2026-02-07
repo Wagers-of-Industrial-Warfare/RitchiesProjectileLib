3c3a7a1b1f79f1aa8d8606156d7fea2a8cc55e78
rbasamoyai <b33gy0sh@gmail.com>
2026-02-06 23:54:15 -0500

    bug: Fixed screen shake not working on NeoForge

--- neoforge/src/main/java/rbasamoyai/ritchiesprojectilelib/neoforge/RPLNeoForgeClient.java
+++ neoforge/src/main/java/rbasamoyai/ritchiesprojectilelib/neoforge/RPLNeoForgeClient.java
@@ -1,33 +1,29 @@
 package rbasamoyai.ritchiesprojectilelib.neoforge;
 
 import net.minecraft.client.Minecraft;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.event.ViewportEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
-import net.neoforged.bus.api.IEventBus;
 import rbasamoyai.ritchiesprojectilelib.RPLClient;
+import rbasamoyai.ritchiesprojectilelib.RitchiesProjectileLib;
 
+@EventBusSubscriber(modid = RitchiesProjectileLib.MOD_ID)
 public class RPLNeoForgeClient {
 
-    public static void init(IEventBus modBus, IEventBus forgeBus) {
-        RPLClient.init();
-
-        forgeBus.addListener(RPLNeoForgeClient::onClientGameTick);
-        //forgeBus.addListener(RPLForgeClient::onCameraSetup);
-        forgeBus.addListener(RPLNeoForgeClient::onPlayerLogOut);
-    }
-
+    @SubscribeEvent
     public static void onClientGameTick(final ClientTickEvent.Post event) {
         Minecraft minecraft = Minecraft.getInstance();
         RPLClient.onEndClientGameTick(minecraft);
     }
 
-    //todo: event is not cancellable
-    /*public static void onCameraSetup(final ViewportEvent.ComputeCameraAngles event) {
-        if (RPLClient.onCameraSetup(event.getCamera(), (float) event.getPartialTick(), new ForgeCameraModifier(event)) && event.isCancelable()) {
-            event.setCanceled(true);
-        }
-    }*/
+    @SubscribeEvent
+    public static void onCameraSetup(final ViewportEvent.ComputeCameraAngles event) {
+        RPLClient.onCameraSetup(event.getCamera(), (float) event.getPartialTick(), new NeoForgeCameraModifier(event));
+    }
 
+    @SubscribeEvent
     public static void onPlayerLogOut(final PlayerEvent.PlayerLoggedOutEvent event) {
         RPLClient.onPlayerLogout(event.getEntity());
     }
--- neoforge/src/main/java/rbasamoyai/ritchiesprojectilelib/neoforge/RitchiesProjectileLibNeoForge.java
+++ neoforge/src/main/java/rbasamoyai/ritchiesprojectilelib/neoforge/RitchiesProjectileLibNeoForge.java
@@ -6,7 +6,6 @@ import net.neoforged.neoforge.event.tick.LevelTickEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.ModLoadingContext;
-import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.bus.api.IEventBus;
 
 import net.neoforged.neoforge.common.NeoForge;
@@ -26,10 +25,6 @@ public class RitchiesProjectileLibNeoForge {
         IEventBus forgeBus = NeoForge.EVENT_BUS;
         forgeBus.addListener(this::onPlayerLogin);
         forgeBus.addListener(this::onServerLevelTick);
-
-        if(FMLEnvironment.dist.isClient()) {
-            RPLNeoForgeClient.init(modBus, forgeBus);
-        }
     }
 
     public void onPlayerLogin(final PlayerLoggedInEvent evt) {
